872ca349c99877383714be1015307d0300f3ba82
zhjch80 <zhjch80@163.com>
2015-02-13 22:19:56 +0800

    no message

---
+++ MoreMeatPlant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MoreMeatPlant
+//
+//  Created by runmobile on 15/2/13.
+//  Copyright (c) 2015年 runmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
