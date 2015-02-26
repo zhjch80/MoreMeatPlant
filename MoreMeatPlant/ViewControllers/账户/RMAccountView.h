d1e8418bbba64db040fb15a1ad543b080e99f7d5
demoker <demok+-*/101311>
2015-02-26 22:40:17 +0800

    no message

---
+++ MoreMeatPlant/ViewControllers/账户/RMAccountView.h
@@ -0,0 +1,13 @@
+//
+//  RMAccountView.h
+//  MoreMeatPlant
+//
+//  Created by 马东凯 on 15/2/26.
+//  Copyright (c) 2015年 runmobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAccountView : UIView
+
+@end
---
+++ MoreMeatPlant/ViewControllers/账户/RMAccountView.m
@@ -0,0 +1,21 @@
+//
+//  RMAccountView.m
+//  MoreMeatPlant
+//
+//  Created by 马东凯 on 15/2/26.
+//  Copyright (c) 2015年 runmobile. All rights reserved.
+//
+
+#import "RMAccountView.h"
+
+@implementation RMAccountView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
